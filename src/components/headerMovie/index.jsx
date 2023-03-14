4d336274d5e840be584267850f22f2432182095d
Joseph Nunan <nunanjoe@gmail.com>
2023-03-14 21:56:01 +0000

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -24,35 +24,9 @@ const styles = {
 const MovieHeader = (props) => {
   const movie = props.movie;
   // Get movies from local storage.
-  const favourite = true; 
-  // const favourite = JSON.parse(localStorage.getItem("title")); 
-  const favourites = localStorage.getItem("favourites"); 
-  console.log("Hello this is favourite -- " + favourites);
-  // const profileEmails = favourites.map((fav) => {
-  //   return {
-  //     name: `${fav.title} ${fav.id}`,
-  //   };
-  // });
-  // console.log("here we go -- " + profileEmails);
-
-  
-  // const profileEmails = profiles.map((profile) => {
-  //   return {
-  //     name: `${profile.name.first} ${profile.name.last}`,
-  //     email: profile.email,
-  //   };
-  // });
-  // console.log(profileEmails);
-  
-  
-  // console.log("Hello this is favouriteID -- " + favouriteId);
+  const favourite = JSON.parse(localStorage.getItem("favourites"));   
+  console.log("Hello this is favourite -- " + favourite);
   console.log("movie id from props " + movie.id);
-  // console.log("Hello this is favourite -- " + json.stringify(favourite));
-
-
-  
-
-
 
   return (
     <Paper component="div" sx={styles.root}>
@@ -60,7 +34,6 @@ const MovieHeader = (props) => {
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
       {favourite ? <Avatar sx={styles.avatar}><FavoriteIcon /></Avatar> : null}
-      {/* {favourite ? <Avatar sx={styles.avatar}><FavoriteIcon /></Avatar> : <Avatar sx={styles.avatar}><ArrowForwardIcon /></Avatar>} */}
       <Typography variant="h4" component="h3">              
         {movie.title}{"   "}
         <a href={movie.homepage}>
@@ -75,41 +48,5 @@ const MovieHeader = (props) => {
     </Paper>
   );
 };
-
-
-// import Avatar from "@mui/material/Avatar";
-
-// <CardHeader
-//         sx={styles.header}
-//         avatar={
-//           movie.favourite ? (
-//             <Avatar sx={styles.avatar}>
-//               <FavoriteIcon />
-//             </Avatar>
-//           ) : null
-//         }
-//         title={
-//           <Typography variant="h5" component="p">
-//             {movie.title}{" "}
-//           </Typography>
-//         }
-//       />
-
-
-// const myFavIconfunction = () => {
-  
-//   // Get movies from local storage.
-//   const movies = JSON.parse(localStorage.getItem("favourites")); 
-//   console.log(movies);
-
-//   const avatar = {
-//     movies.favourite ? (
-//       <Avatar sx={styles.avatar}>
-//         <FavoriteIcon />
-//       </Avatar>
-//     ) : null
-//   };
-//   return avatar;
-// }
 
 export default MovieHeader;
